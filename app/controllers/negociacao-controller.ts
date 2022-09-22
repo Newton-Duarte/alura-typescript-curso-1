9f0561166923d0bd3507d3a3036703fb1643e15f
Newton Duarte <newton_@hotmail.com>
2022-09-22 09:27:31 -0300

    feat: implement add method, create negociacao method and limpa formulario method

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,7 +1,9 @@
+import { Negociacao } from "../models/negociacao.js";
+
 export class NegociacaoController {
-  private inputData;
-  private inputQuantidade;
-  private inputValor;
+  private inputData: HTMLInputElement;
+  private inputQuantidade: HTMLInputElement;
+  private inputValor: HTMLInputElement;
 
   constructor() {
     this.inputData = document.querySelector('#data');
@@ -10,8 +12,24 @@ export class NegociacaoController {
   }
 
   adiciona() {
-    console.log(this.inputData)
-    console.log(this.inputQuantidade)
-    console.log(this.inputValor)
+    const negociacao = this.criaNegociacao();
+    console.log(negociacao);
+    console.log(negociacao.volume);
+    this.limpaFormulario();
+  }
+
+  criaNegociacao(): Negociacao {
+    const exp = /-/g;
+    const data = new Date(this.inputData.value.replace(exp, ','));
+    const quantidade = parseInt(this.inputQuantidade.value);
+    const valor = parseFloat(this.inputValor.value);
+    return new Negociacao(data, quantidade, valor);
+  }
+
+  limpaFormulario(): void {
+    this.inputData.value = '';
+    this.inputQuantidade.value = '1';
+    this.inputValor.value = '0.0';
+    this.inputData.focus();
   }
 }
