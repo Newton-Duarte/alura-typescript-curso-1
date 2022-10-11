6fcb4f8ed20e09fe7627445a46affbefd4e6d536
Newton Duarte <newton_@hotmail.com>
2022-10-11 09:32:34 -0300

    feat: transform view class into abstract class and template method into abstract method

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,7 +1,14 @@
-export class View {
+export abstract class View<T> {
   protected elemento: HTMLElement;
 
   constructor(seletor: string) {
     this.elemento = document.querySelector(seletor);
+  }
+
+  abstract template(mensagem: T): string;
+
+  update(mensagem: T): void {
+    const template = this.template(mensagem);
+    this.elemento.innerHTML = template;
   }
 }
