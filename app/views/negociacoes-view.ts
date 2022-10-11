c15b9c52ccdcde2b6bc6facdb3f31ce09754534c
Newton Duarte <newton_@hotmail.com>
2022-10-11 09:33:00 -0300

    feat: implement view abstract class

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,7 +1,7 @@
 import { Negociacoes } from "../models/negociacoes.js";
 import { View } from "./view.js";
 
-export class NegociacoesView extends View {
+export class NegociacoesView extends View<Negociacoes> {
   template(model: Negociacoes): string {
     return `
       <table class="table table-hover table-bordered">
@@ -25,9 +25,5 @@ export class NegociacoesView extends View {
         </tbody>
       </table>
     `;
-  }
-
-  update(model: Negociacoes): void {
-    this.elemento.innerHTML = this.template(model);
   }
 }
