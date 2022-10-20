ea2eaf69870e0cd8b9a16f722562c78b89da0443
Newton Duarte <newton_@hotmail.com>
2022-10-20 09:21:43 -0300

    feat: check if form element exist

--- app/app.ts
+++ app/app.ts
@@ -3,7 +3,12 @@ import { NegociacaoController } from "./controllers/negociacao-controller.js";
 const negociacao = new NegociacaoController();
 
 const form = document.querySelector('.form');
-form.addEventListener('submit', event => {
-  event.preventDefault();
-  negociacao.adiciona();
-})
+
+if (form) {
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+    negociacao.adiciona();
+  });
+} else {
+  throw Error('Elemento form não existe.');
+}
