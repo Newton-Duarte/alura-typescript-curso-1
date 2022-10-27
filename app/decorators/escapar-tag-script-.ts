5fdcbe156b8492c7490c7cdf36d400ec5e7a5bc7
Newton Duarte <newton_@hotmail.com>
2022-10-27 09:39:16 -0300

    feat: add escapar tag script decorator

---
+++ app/decorators/escapar-tag-script-.ts
@@ -0,0 +1,17 @@
+export function escaparTagScript(
+  target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor
+) {
+  const metodoOriginal = descriptor.value;
+  descriptor.value = function(...args: any[]) {
+    let retorno = metodoOriginal.apply(this, args);
+    if (typeof retorno === 'string') {
+      console.log(`@escaparTagScript em ação na classe ${this.constructor.name} para o método ${propertyKey}`);
+      retorno = retorno.replace(/<script>[\s\S]*?<\/script>/, '');
+    }
+    return retorno;
+  }
+
+  return descriptor;
+}
