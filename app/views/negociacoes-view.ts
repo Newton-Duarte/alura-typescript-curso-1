7aa2de0b4acf954e9ffd0fbca29eb89eabaf9aba
Newton Duarte <newton_@hotmail.com>
2022-10-27 09:40:27 -0300

    feat: use escapar tag script decorator

--- app/views/negociacoes-view.ts
+++ app/views/negociacoes-view.ts
@@ -1,7 +1,9 @@
+import { escaparTagScript } from "../decorators/escapar-tag-script-.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { View } from "./view.js";
 
 export class NegociacoesView extends View<Negociacoes> {
+  @escaparTagScript
   protected template(model: Negociacoes): string {
     return `
       <table class="table table-hover table-bordered">
