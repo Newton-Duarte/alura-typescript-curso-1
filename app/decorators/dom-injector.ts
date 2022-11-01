55d19c26e2dbe245631aeedc8b51a657578c070d
Newton Duarte <newton_@hotmail.com>
2022-11-01 09:15:05 -0300

    feat: add decorator dom injector

---
+++ app/decorators/dom-injector.ts
@@ -0,0 +1,27 @@
+export function domInjector(seletor: string) {
+  return function(target: any, propertyKey: string) {
+    console.log(
+      `Modificando prototype ${target.constructor.name} e 
+      adicionando getter para a propriedade ${propertyKey}`
+    );
+
+    let elemento: HTMLElement;
+
+    const getter = function() {
+      if (!elemento) {
+        elemento = document.querySelector(seletor) as HTMLElement;
+        // elemento = <HTMLElement>document.querySelector(seletor); // outra forma de fazer cast
+        console.log(
+          `Buscando elemento do DOM com o seletor ${seletor} 
+          para injetar em ${propertyKey}`
+        );
+      }
+
+      return elemento;
+    }
+
+    Object.defineProperty(target, propertyKey, {
+      get: getter
+    })
+  }
+}
